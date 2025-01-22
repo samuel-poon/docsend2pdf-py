85c3d2b30464431171bfa75d93bb592bf20d9463
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2025-01-22 10:22:30 -0500

    Make README long_description

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='docsend2pdf',
     description='Python wrapper for DocSend2PDF',
+    long_description=open('README.md').read(),
     version='0.0.1',
     python_requires='>=3.8',
     install_requires=[
