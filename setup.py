1fabdb23bda3d02f66c2ce9e2078b9d293539380
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2025-01-22 10:02:11 -0500

    Add description

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='docsend2pdf',
+    description='Python wrapper for DocSend2PDF',
     version='0.0.1',
     python_requires='>=3.8',
     install_requires=[
