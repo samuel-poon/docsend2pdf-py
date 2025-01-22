9b6f8f6bccf261fe5a3d73bcd0793cec4f85019c
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2025-01-22 10:23:10 -0500

    Add more setup args

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='docsend2pdf',
     description='Python wrapper for DocSend2PDF',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/samuel-poon/docsend2pdf-py',
     version='0.0.1',
     python_requires='>=3.8',
     install_requires=[
